f65c9d7a684f4931737b962ad509ed3184e6829e
jstalex <90794457+jstalex@users.noreply.github.com>
2022-11-14 22:32:54 +0300

    added working sorting

--- main.asm
+++ main.asm
@@ -4,42 +4,42 @@
     # s0 = base adress, s1 - i, s2 - j
 
     li s1, 0
-    li s2, 1
+    li s2, 0
     la s0, arr
-    li s4, 4 # len of array
-    # t0 = *arr[i]
-    # t1 = arr[i]
+    li s4, 3 # len of array
+    li s5, 3 # for j
+    # t0 = *arr[j+1]
+    # t1 = arr[j+1]
     # t2 = *arr[j]
-    # t3 = *arr[j]  
+    # t3 = arr[j]  
     for:
         bge s1, s4, done
-        slli t0, s1, 2 # t0 = i*4
-        add t0, t0, s0 # get arr[i] adress
-        lw t1, 0(t0) #get arr[i] value
-
         for_inter:
-            bge s2, s4, done_inter
+            sub s5, s4, s1 # jmax = imax - i
+            bge s2, s5, done_inter
+
             slli t2, s2, 2 # t2 = j*4
             add t2, t2, s0 # get arr[j] adress
+
+            addi t0, t2, 4 # t0 = *arr[j+1]
+
             lw t3, 0(t2) # get arr[j] value
             lw t1, 0(t0) #get arr[i] value
         
-            blt t3, t1, change
-            L1:
+            bge t3, t1, L1
+            mv a2, t3 # save t3 old value
+            mv t3, t1
+            mv t1, a2
+            L1: 
+            
             sw t1, 0(t0) # save new values
             sw t3, 0(t2) # save new values
 
-            addi s2, s2, 1
+            addi s2, s2, 1 #j++
             j for_inter
         done_inter:
 
+        add s2, zero, zero # j = 0
         addi s1, s1, 1 # i += 1
         j for 
-    done: j L2
-
-    change:
-        mv t4, t3 # save t3 old value
-        mv t3, t1
-        mv t1, a2
-        j L1
-    L2:    
+    done:
