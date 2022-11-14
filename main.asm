08938087dbbfba33c3effb198104cd08c01cdcc1
jstalex <90794457+jstalex@users.noreply.github.com>
2022-11-14 10:31:56 +0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -2,14 +2,20 @@
     arr: .word 3, 1, 4, 2 # source data
 .text
     # s0 = base adress, s1 - i, s2 - j
-    la s0, arr
+
     li s1, 0
-    li s2, 0
-    li s4, 5 # len of array
+    li s2, 1
+    la s0, arr
+    li s4, 4 # len of array
     # t0 = *arr[i]
     # t1 = arr[i]
     # t2 = *arr[j]
-    # t3 = *arr[j]
+    # t3 = *arr[j]  
+    for:
+        bge s1, s4, done
+        slli t0, s1, 2 # t0 = i*4
+        add t0, t0, s0 # get arr[i] adress
+        lw t1, 0(t0) #get arr[i] value
 
         for_inter:
             bge s2, s4, done_inter
